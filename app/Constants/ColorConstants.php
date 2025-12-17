82ff8ffeefeea962c5d216b3712806779e6f08c3
surya.permana <suryaintpermana@gmail.com>
2025-12-17 14:00:27 +0700

    [add] add constants

---
+++ app/Constants/ColorConstants.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Constants;
+
+class ColorConstants
+{
+    const COLORS = [
+        'CYAN' => '#06b6d4',
+        'GREEN' => '#10b981',
+        'BLUE' => '#3b82f6',
+        'INDIGO' => '#6366f1',
+        'PURPLE' => '#d946ef',
+        'RED' => '#f43f5e',
+        'YELLOW' => '#84cc16',
+    ];
+}
