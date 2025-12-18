f778b84030c9ff3aa3fcb078dd2c1ec3883599b9
surya.permana <suryaintpermana@gmail.com>
2025-12-18 13:10:58 +0700

    [add] add model and migration net worth asset

---
+++ database/migrations/2025_12_18_060907_create_net_worth_assets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('net_worth_assets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('asset_id')->constrained()->onDelete('cascade');
+            $table->date('transaction_date');
+            $table->unsignedInteger('nominal')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('net_worth_assets');
+    }
+};
